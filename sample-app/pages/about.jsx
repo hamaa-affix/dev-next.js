bd3f4539360a707165eea63d69991b260f440ecf
hamaa-affix <hamaagive1108@gmail.com>
2021-04-11 09:47:08 +0900

    headerの追加

--- sample-app/pages/about.jsx
+++ sample-app/pages/about.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { MainDocument } from '../components/MainDocument';
 import { Footer } from "../components/Footer";
 import styles from '../styles/Home.module.css'
+import { Header } from '../components/Header';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <title>about page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
       <MainDocument page="about" title="about" />
       <Footer />
     </div>
