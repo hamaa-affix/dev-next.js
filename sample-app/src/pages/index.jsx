0d67f2edb30e451e8244a380ec16a3baefe9c5b2
hamaa-affix <hamaagive1108@gmail.com>
2021-04-26 09:07:58 +0900

    useEffect practice

--- sample-app/src/pages/index.jsx
+++ sample-app/src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import Head from 'next/head'
 import { MainDocument } from "../components/MainDocument/MainDocument"
 import { Footer } from "../components/Footer/index";
@@ -8,11 +8,12 @@ import { Header } from 'src/components/Header/index';
 
 export default function Home() {
   //useCallback ->　再レンダリングする際に再生成することがなくなる。コンポーネントのパフォーマンスを維持してくれる。
-  const handleClick = useCallback((e) => {
-    console.log(e);
-    e.preventDefault();
-    alert(1234);
-  }, []);
+  // const handleClick = useCallback((e) => {
+  //   console.log(e);
+  //   e.preventDefault();
+  //   alert(1234);
+  // }, []);
+  
 
   return (
     <div className={styles.container}>
@@ -20,7 +21,7 @@ export default function Home() {
         <title>index page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <button onClick={handleClick}>ボタン</button>
+      {/* <button onClick={handleClick}>ボタン</button> */}
       <Header />
       <MainDocument page="index" title="index" />
       <Footer />
